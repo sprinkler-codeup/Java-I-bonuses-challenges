9d4ebdfdceecc4648719cc262cdf6fb840b6cd22
megansimmons <simmons.megan584@gmail.com>
2019-03-21 14:32:39 -0500

    first commit

---
+++ src/megan_bonus.java
@@ -0,0 +1,29 @@
+public class megan_bonus {
+    public static void main(String[] args) {
+
+
+
+
+//        1) create three variables, start, end, and sum. Use a loop to find the summation of all the numbers between start and end (inclusive)
+
+        int start = 1;
+        int end = 10;
+        int sum = 0;
+//        for(int i = start; i <= end; i+=1 ){
+//            sum = sum + i;
+//        System.out.println(sum);
+//        }
+
+
+
+
+
+
+//        2) write a loop which will output all the prime numbers (evenly divisible by only 1 and themselves) between 10 and 10000
+//        3) output a table representing the binary representations of the powers of 2
+//        decimal       |       binary
+//        1     |     00000001
+//        2     |     00000010
+//        4     |     00000100 (edited)
+    }
+}
