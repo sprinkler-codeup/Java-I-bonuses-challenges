8d3e83fa3dc11ad54e60297ee9bf0318677b1c77
megansimmons <simmons.megan584@gmail.com>
2019-03-22 08:47:48 -0500

    second bonus complete

--- src/megan_bonus.java
+++ src/megan_bonus.java
@@ -6,9 +6,9 @@ public class megan_bonus {
 
 //        1) create three variables, start, end, and sum. Use a loop to find the summation of all the numbers between start and end (inclusive)
 
-        int start = 1;
-        int end = 10;
-        int sum = 0;
+//        int start = 1;
+//        int end = 10;
+//        int sum = 0;
 //        for(int i = start; i <= end; i+=1 ){
 //            sum = sum + i;
 //        System.out.println(sum);
@@ -20,6 +20,42 @@ public class megan_bonus {
 
 
 //        2) write a loop which will output all the prime numbers (evenly divisible by only 1 and themselves) between 10 and 10000
+
+
+
+    // From Stack Overflow
+
+        int max = 100;
+
+        System.out.println("Generate Prime numbers between 1 and " + max);
+
+        // loop through the numbers one by one
+        for (int i = 1; i<max; i++) {
+
+            boolean isPrimeNumber = true;
+
+            // check to see if the number is prime
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrimeNumber = false;
+                    break; // exit the inner for loop
+                }
+            }
+
+            // print the number if prime
+            if (isPrimeNumber) {
+                System.out.print(i + " ");
+            }
+        }
+
+
+//       long prime = 0;
+//        for(long i = 0; i <= 13; i+=1){
+//            if(i % i == 0){
+//                System.out.println(i);
+//            }
+//        }
+
 //        3) output a table representing the binary representations of the powers of 2
 //        decimal       |       binary
 //        1     |     00000001
@@ -27,3 +63,4 @@ public class megan_bonus {
 //        4     |     00000100 (edited)
     }
 }
+
